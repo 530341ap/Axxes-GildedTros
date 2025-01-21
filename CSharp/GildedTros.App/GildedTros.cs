156ad9a98e14981aeaabf3ab30aa5b6754754a1e
Aïssa <aissa.peters@gmail.com>
2025-01-21 13:17:33 +0100

    Write tests

--- CSharp/GildedTros.App/GildedTros.cs
+++ CSharp/GildedTros.App/GildedTros.cs
@@ -9,8 +9,9 @@ namespace GildedTros.App
         public static string legendaryItem = "B-DAWG Keychain";
         public static string goodWine = "Good Wine";
         public static string[] backstagePasses = { "Backstage passes for Re:factor", "Backstage passes for HAXX" };
+        public static string[] smellyItems = { "Duplicate Code", "Long Methods", "Ugly Variable Names" };
 
-        public GildedTros(IList<Item> Items)
+        public GildedTros(IList<Item> Items)  
         {
             this.Items = Items;
         }
@@ -54,82 +55,6 @@ namespace GildedTros.App
                 }
                 item.SellIn = item.SellIn - 1;
             }
-            //for (var i = 0; i < Items.Count; i++)
-            //{
-            //    if (Items[i].Name != "Good Wine"
-            //        && Items[i].Name != "Backstage passes for Re:factor"
-            //        && Items[i].Name != "Backstage passes for HAXX")
-            //    {
-            //        if (Items[i].Quality > 0)
-            //        {
-            //            if (Items[i].Name != "B-DAWG Keychain")
-            //            {
-            //                Items[i].Quality = Items[i].Quality - 1;
-            //            }
-            //        }
-            //    }
-            //    else
-            //    {
-            //        if (Items[i].Quality < 50)
-            //        {
-            //            Items[i].Quality = Items[i].Quality + 1;
-
-            //            if (Items[i].Name == "Backstage passes for Re:factor"
-            //            || Items[i].Name == "Backstage passes for HAXX")
-            //            {
-            //                if (Items[i].SellIn < 11)
-            //                {
-            //                    if (Items[i].Quality < 50)
-            //                    {
-            //                        Items[i].Quality = Items[i].Quality + 1;
-            //                    }
-            //                }
-
-            //                if (Items[i].SellIn < 6)
-            //                {
-            //                    if (Items[i].Quality < 50)
-            //                    {
-            //                        Items[i].Quality = Items[i].Quality + 1;
-            //                    }
-            //                }
-            //            }
-            //        }
-            //    }
-
-            //    if (Items[i].Name != "B-DAWG Keychain")
-            //    {
-            //        Items[i].SellIn = Items[i].SellIn - 1;
-            //    }
-
-            //    if (Items[i].SellIn < 0)
-            //    {
-            //        if (Items[i].Name != "Good Wine")
-            //        {
-            //            if (Items[i].Name != "Backstage passes for Re:factor"
-            //                && Items[i].Name != "Backstage passes for HAXX")
-            //            {
-            //                if (Items[i].Quality > 0)
-            //                {
-            //                    if (Items[i].Name != "B-DAWG Keychain")
-            //                    {
-            //                        Items[i].Quality = Items[i].Quality - 1;
-            //                    }
-            //                }
-            //            }
-            //            else
-            //            {
-            //                Items[i].Quality = Items[i].Quality - Items[i].Quality;
-            //            }
-            //        }
-            //        else
-            //        {
-            //            if (Items[i].Quality < 50)
-            //            {
-            //                Items[i].Quality = Items[i].Quality + 1;
-            //            }
-            //        }
-            //    }
-            //}
         }
 
         private bool IsIncreasingItem(string name)
--- CSharp/GildedTros.App/GildedTrosTest.cs
+++ CSharp/GildedTros.App/GildedTrosTest.cs
@@ -6,12 +6,130 @@ namespace GildedTros.App
     public class GildedTrosTest
     {
         [Fact]
-        public void foo()
+        public void QualityLowersOneWhenSellInPositive()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 1, Quality = 2 } };
+            GildedTros app = new GildedTros(Items);
+            app.UpdateQuality();
+            Assert.Equal(1, Items[0].Quality);
+        }
+
+        [Fact]
+        public void QualityLowersTwoWhenSellInNegative()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = -1, Quality = 2 } };
+            GildedTros app = new GildedTros(Items);
+            app.UpdateQuality();
+            Assert.Equal(0, Items[0].Quality);
+        }
+
+        [Fact]
+        public void QualitySmellyItemsLowersTwoWhenSellInPositive()
+        {
+            IList<Item> Items = new List<Item> { new Item {Name = "Duplicate Code", SellIn = 3, Quality = 6},
+                new Item {Name = "Long Methods", SellIn = 3, Quality = 6},
+                new Item {Name = "Ugly Variable Names", SellIn = 3, Quality = 6} };
+            GildedTros app = new GildedTros(Items);
+            app.UpdateQuality();
+            Assert.Equal(4, Items[0].Quality);
+            Assert.Equal(4, Items[1].Quality);
+            Assert.Equal(4, Items[2].Quality);
+        }
+
+        [Fact]
+        public void QualitySmellyItemsLowersFourWhenSellInNegative()
+        {
+            IList<Item> Items = new List<Item> { new Item {Name = "Duplicate Code", SellIn = -1, Quality = 6},
+                new Item {Name = "Long Methods", SellIn = -1, Quality = 6},
+                new Item {Name = "Ugly Variable Names", SellIn = -1, Quality = 6} };
+            GildedTros app = new GildedTros(Items);
+            app.UpdateQuality();
+            Assert.Equal(2, Items[0].Quality);
+            Assert.Equal(2, Items[1].Quality);
+            Assert.Equal(2, Items[2].Quality);
+        }
+
+        [Fact]
+        public void QualityNeverBelowZero()
         {
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
             GildedTros app = new GildedTros(Items);
             app.UpdateQuality();
-            Assert.Equal("fixme", Items[0].Name);
+            Assert.Equal(0, Items[0].Quality);
+        }
+
+        [Fact]
+        public void KeychainNeverChangesQuality()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "B-DAWG Keychain", SellIn = 0, Quality = 80 }, 
+                new Item { Name = "B-DAWG Keychain", SellIn = -1, Quality = 80 } };
+            GildedTros app = new GildedTros(Items);
+            app.UpdateQuality();
+            Assert.Equal(80, Items[0].Quality);
+            Assert.Equal(80, Items[1].Quality);
+        }
+
+        [Fact]
+        public void QualityGoodWineIncreasesOneWhenSellInPositive()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Good Wine", SellIn = 1, Quality = 2 } };
+            GildedTros app = new GildedTros(Items);
+            app.UpdateQuality();
+            Assert.Equal(3, Items[0].Quality);
+        }
+
+        [Fact]
+        public void QualityGoodWineIncreasesTwoWhenSellInNegative()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Good Wine", SellIn = -1, Quality = 2 } };
+            GildedTros app = new GildedTros(Items);
+            app.UpdateQuality();
+            Assert.Equal(4, Items[0].Quality);
+        }
+
+        [Fact]
+        public void QualityGoodWineNeverAbove50()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Good Wine", SellIn = -1, Quality = 49 } };
+            GildedTros app = new GildedTros(Items);
+            app.UpdateQuality();
+            Assert.Equal(50, Items[0].Quality);
+        }
+
+        [Fact]
+        public void QualityBackstagePassesNeverAbove50()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes for Re:factor", SellIn = 12, Quality = 49 } };
+            GildedTros app = new GildedTros(Items);
+            app.UpdateQuality();
+            Assert.Equal(50, Items[0].Quality);
+        }
+
+        [Fact]
+        public void QualityBackstagePassesIncreasesTwoWhenSellInUnder11()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes for Re:factor", SellIn = 10, Quality = 2 } };
+            GildedTros app = new GildedTros(Items);
+            app.UpdateQuality();
+            Assert.Equal(4, Items[0].Quality);
+        }
+
+        [Fact]
+        public void QualityBackstagePassesIncreasesThreeWhenSellInUnder6()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes for Re:factor", SellIn = 5, Quality = 2 } };
+            GildedTros app = new GildedTros(Items);
+            app.UpdateQuality();
+            Assert.Equal(5, Items[0].Quality);
+        }
+
+        [Fact]
+        public void QualityBackstagePassesZeroWhenPastSellInDate()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes for Re:factor", SellIn = 0, Quality = 50 } };
+            GildedTros app = new GildedTros(Items);
+            app.UpdateQuality();
+            Assert.Equal(0, Items[0].Quality);
         }
     }
 }
